ce782cd6314a37e86bf555896a1ce369e6e6535e
rajvora <rajvora1998@gmail.com>
2023-12-05 04:16:09 -0500

    [Frontend] Some console errors fixes on settings page

--- opencti-platform/opencti-front/src/components/ColorPickerField.jsx
+++ opencti-platform/opencti-front/src/components/ColorPickerField.jsx
@@ -62,10 +62,13 @@ const ColorPickerField = (props) => {
     }
   };
 
+  const { value, ...otherProps } = fieldToTextField(props);
+
   return (
     <>
       <MuiTextField
-        {...fieldToTextField(props)}
+        {...otherProps}
+        value={value ?? ''}
         error={!isNil(meta.error)}
         helperText={!isNil(meta.error) ? meta.error : props.helperText}
         ref={anchorEl}
